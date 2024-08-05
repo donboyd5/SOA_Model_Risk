8db6eb83b46bddcf3e7bfaa4519786c27dc0cbc0
Don Boyd <donboyd5@gmail.com>
2024-08-05 06:20:46 -0400

    move portfolio log returns function to functions_finance.R file
    CAUTION: DO NOT MERGE UNTIL AFTER MEETING!

---
+++ report/functions_finance.R
@@ -0,0 +1,30 @@
+
+
+lrport <- function(lrassets, weights){
+  # lrassets is a matrix of log returns of assets where columns are assets, rows are timepoints
+  # weights is a vector of weights that sum to 1
+  
+  # argument checking
+  if (abs(sum(weights) - 1) > .Machine$double.eps^0.5) {
+    stop("Error: The weights must sum to 1.")
+  }
+  
+  lrassets <- as.matrix(lrassets)
+  port_aret <- (exp(lrassets) -1) %*% weights # arithmetic return of portfolio
+  log(1 + port_aret) |> as.vector()  # log returns of the portfolio
+}
+
+
+runslv <- function(data, start, end, modtype="leverage"){
+  # get the demeaned log return from a data subset and estimate an slv model
+  # data must have date and lr columns
+  
+  vec <- data |> 
+    filter(date >= as.Date(start), date <= as.Date(end)) |> 
+    filter(!is.na(lr)) |> 
+    mutate(lrc=lr - mean(lr)) |> 
+    pull(lrc)
+  
+  mod <- estimate_parameters(vec, model = modtype, silent = TRUE)
+  mod
+}
